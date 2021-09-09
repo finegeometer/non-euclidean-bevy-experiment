c553a1c714981bb7c27c13eb40e6a1ac24a0f5cd
finegeometer <finegeometer@gmail.com>
2021-09-09 16:45:19 -0700

    Module camera has reached its final form. 7127.

--- bevy_render_spherical/src/camera/mod.rs
+++ bevy_render_spherical/src/camera/mod.rs
@@ -1,12 +1,9 @@
-mod active_cameras;
-#[allow(clippy::module_inception)]
-mod camera {
-    pub use bevy_render::camera::{camera_system, Camera, DepthCalculation};
-}
 mod projection;
 mod visible_entities;
 
-pub use active_cameras::*;
-pub use camera::*;
+pub use bevy_render::camera::{
+    active_cameras_system, camera_system, ActiveCamera, ActiveCameras, Camera, DepthCalculation,
+};
+
 pub use projection::*;
 pub use visible_entities::*;
